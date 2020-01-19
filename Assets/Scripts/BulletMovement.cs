e7cbb19720f4b0eaf2bf1da0a6a84979bf67b9ef
Pofay <gianCarlo.gilos@outlook.com>
2020-01-19 20:16:17 +0800

    Can now perform shooting

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float runSpeed = 5f;
+
+    float verticalMove = 1f;
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var velocity = new Vector2(0f, verticalMove);
+        rigidBody.velocity = velocity * runSpeed;
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    private Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponentInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+
+}

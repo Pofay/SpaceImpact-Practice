0834e7a349fbb9ced2b77ef3c6401556d8e5b342
Pofay <gianCarlo.gilos@gmail.com>
2020-02-07 15:38:45 +0800

    Added Starfield Background

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    public float scrollSpeed = 10f;
+    private MeshRenderer renderer;
+    private Material material;
+
+    private void Start()
+    {
+        //this.renderer = GetComponent<MeshRenderer>();
+        //this.material = renderer.material; 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        var renderer = GetComponent<MeshRenderer>();
+        var material = renderer.material;
+        var offset = material.mainTextureOffset;
+
+        offset.x += Time.deltaTime / scrollSpeed;
+
+        material.mainTextureOffset = offset;
+    }
+}
